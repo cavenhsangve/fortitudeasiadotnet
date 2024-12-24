f32d28774419ea1e04c4c00cb903dc384111e0b6
cavenhsangve <jadencaven@gmail.com>
2024-12-25 00:44:06 +0800

    Revert "systemchanges"
    This reverts commit 5610d03cddc7a21ab433404adf0dabef75039f6e.

--- dotnettest/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ dotnettest/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab2535dbb2b0563b2749bfc909e5905b03da805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923c0a4e477b05936d3ac67446e73b55a4f4212b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
